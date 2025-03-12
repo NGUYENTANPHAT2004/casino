9201e0c0db25fa5df9f4238b802c716e4c443403
NGUYENTANPHAT2004 <tttp1704@gmail.com>
2025-03-13 04:58:09 +0700

    Update db.js

--- ez88-backend/src/config/db.js
+++ ez88-backend/src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/ez88');
+        await mongoose.connect('MONGO_URI=mongodb+srv://phat1z:phatdz17052004@ez88-cluster.mongodb.net/ez88?retryWrites=true&w=majority');
         console.log("MongoDB Connected");
     } catch (error) {
         console.error("MongoDB Connection Failed:", error);
